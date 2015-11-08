a1d35b05aa07f2cd77785b7c7b802878f1890e28
maximsolopin <maxim.solopin@gmail.com>
2015-11-08 14:09:12 +0300

    Доделаны стили таблиц и сделана валидация на форме комментариев

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,30 @@
 class CommentsController < ApplicationController
-	
+  before_action :set_article, only: [:create, :destroy, :show]
+
   http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
- 
+
   def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    @comment = @article.comments.new(comment_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'articles/show'
+    end
   end
- 
+
   def destroy
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
   end
- 
+
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :article_id)
+  end
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
 end
