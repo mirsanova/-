ff87d25350282830dcc14db2b7ef4ee56d40d136
Ann <anna.annanx@yandex.ru>
2015-11-17 09:53:24 +0300

    Добавила таблицу categories

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     resources :deliveries
   end
 
+  
+
   # post "/api", to: "api#calculate_ems"
 
   # The priority is based upon order of creation: first created -> highest priority.
